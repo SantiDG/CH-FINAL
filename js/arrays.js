08d814d8a893aa3628d94253ac78c33203965721
SantiDG <79757883+SantiDG@users.noreply.github.com>
2021-10-19 19:10:56 -0300

    CRUD creando productos y descuentos

--- js/arrays.js
+++ js/arrays.js
@@ -1,1 +1,57 @@
 
+
+class Producto {
+
+    constructor(nombre, precio, unidad) {
+
+        this.nombre = nombre.toLowerCase()
+        this.precio = precio
+        this.unidad = unidad
+
+ //       const date = date()
+
+    }
+}
+
+const productos = []
+
+//Metodo que retorna la lista de productos
+const getAll = () => {
+    return productos;
+}
+
+//Metodo para agregar un producto a la lista
+const create = (producto) => {
+    productos.push(producto)
+
+}
+
+//Crear un nuevo producto con instancia de la clase Producto
+const producto1 = new Producto('multiS', 6000, 56)
+const producto2 = new Producto('multiA', 5500, 36)
+
+
+
+//Agegar un producto a la lista
+create(producto1)
+create(producto2)
+
+console.log(productos)
+
+
+
+/*
+//Metodo para hallar un producto por nombre
+const findOne = (codigo) => {
+
+    codigo = codigo.toLowerCase()
+
+    const descuento = descuentos.find(decuento => descuento.codigo == codigo);
+
+    if (!descuento) {
+        throw new Error(`No existe ${codigo}`);
+    }
+
+    return descuento
+}
+*/
--- js/objects.js
+++ js/objects.js
@@ -1,57 +1,152 @@
 
-const product1 = new Object ({
 
-    nombre:'MultiA',
-    valor: 5000,
-    date: 0,
-    unidad: 000,
 
-})
+class Producto {
 
-const product2 = new Object ({
+    constructor(nombre, precio, unidad) {
 
-    nombre:'MultiS',
-    valor: 6000,
-    date: 0,
-    unidad: 000,
+        this.nombre = nombre.toLowerCase()
+        this.precio = precio
+        this.unidad = unidad
+
+ //       const date = date()
+
+    }
+}
+
+const productos = []
+
+//Metodo que retorna la lista de productos
+const getAllProducts = () => {
+    return productos;
+}
+
+//Metodo para agregar un producto a la lista
+const createProducts = (producto) => {
+    productos.push(producto)
+
+}
+
+//Crear un nuevo producto con instancia de la clase Producto
+const producto1 = new Producto('multiS', 6000, 56)
+const producto2 = new Producto('multiA', 5500, 36)
+
+
+
+//Agegar un producto a la lista
+createProducts(producto1)
+createProducts(producto2)
+
+const findOneProd = (nombre) => {
+
+    nombre = nombre.toLowerCase()
+
+    const producto = productos.find(producto => producto.nombre == nombre);
+
+    if (!producto) {
+        throw new Error(`No existe ${nombre}`);
+    }
+
+    return producto
+}
+
+
+class Descuento {
+
+    constructor(codigo, valor) {
+
+        this.codigo = codigo.toUpperCase()
+        this.valor = valor
+
+    }
+}
+
+const descuentos = []
+
+
+//Metodo que retorna la lista de descuentos
+const getAllDesc = () => {
+    return descuentos;
+}
+
+//Metodo para agregar un perro a la lista
+const createDesc = (descuento) => {
+    descuentos.push(descuento)
+
+}
+
+//Metodo para hallar un perro por nombre
+const findOneDesc = (codigo) => {
+
+    codigo = codigo.toUpperCase()
+
+    const descuento = descuentos.find(descuento => descuento.codigo == codigo);
+
+    if (!descuento) {
+        throw new Error(`No existe ${codigo}`);
+    }
+
+    return descuento
+}
+
+//Metodo para eliminar un perro
+const remove = (codigo) => {
+
+codigo = codigo.toUpperCase()
+
+const descuento = findOne(codigo)
+
+const index = descuentos.indexOf(descuento)
+
+descuentos.splice (index, 1)
+
+}
+
+//Metodo para modificar perritos
+
+const update = (codigo, valor) =>{
+
+    const descuento = findOne(codigo)
+    descuento.valor = valor
     
+}
 
 
-})
+//Paso 1
+//Crear un nuevo perro con instancia de la clase Perro
+const descuento1 = new Descuento('DIADELAMIGO', 0.8)
+const descuento2 = new Descuento('NOMBREINFLUENCER', 0.95)
+const descuento3 = new Descuento('FAMILIARDM', 0.50)
+const descuento4 = new Descuento('CLIENTEFRECUENTE', 0.85)
 
 
-console.log(product1);
+//Paso 2
+//Agegar un perro a la lista
+createDesc(descuento1)
+createDesc(descuento2)
+createDesc(descuento3)
+createDesc(descuento4)
 
-console.log(product2);
+console.log(getAllDesc());
+console.log(getAllProducts());
 
 
-const serial1 = (unidad) => {
+//findOneDesc('DIADELAMIGO');
+//findOneProd('multis');
 
-    product1.date = Date() 
+const AplicarDesc = (nombre, codigo) => {
 
-    product1.unidad = unidad
+    const producto = findOneProd(nombre)
+
+    let precio = producto.precio
+
+    const descuento = findOneDesc(codigo)
+
+    let valor = descuento.valor
+
+    return (precio*valor)
 
 }
 
-const serial2 = (unidad) => {
-
-    product2.date = Date()
-
-    product2.unidad = unidad
-
-}
-
-serial1(54);
-
-serial2(67);
-
-
-
-console.log(product1);
-
-console.log(product2);
-
-
-
-
+console.log(AplicarDesc('multiA','DIADELAmIGO'));
 
